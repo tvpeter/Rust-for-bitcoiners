996c8160e3ad2d2690c8fa518cbeba65f4d2dcfc
Vihiga Tyonum <withtvpeter@gmail.com>
2024-06-27 10:39:54 +0100

    Complete shift alphabet and cipher fns
    - Add check given character and shift by number
    - apply rem_euclid to return positive modulus
    For caesar-cipher:
    - convert given message to bytes
    - iterate and return the shifted alphabet
    - convert the encrypted message back to string
    
    [Ticket: X]

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,15 @@ fn main() {
 
     let message = &args[0];
     let shift: i32 = match args[1].parse::<i32>() {
-        Ok(n) if (1..=26).contains(&n) => n,
+        Ok(n)  => n, //accept any number so long as it can be parsed as i32
         _ => {
             println!("{} Invalid Input", "Error:".red().bold());
             std::process::exit(1);
         }
     };
-    // let encrypted_message = caesar_cipher(message, shift);
-    // println!("{}", encrypted_message); // Don not change this
+    
+    let encrypted_message = caesar_cipher(message, shift);
+    println!("{}", encrypted_message.green()); // Don not change this
     
 }
 
@@ -40,8 +41,16 @@ fn shift_alphabet(c: u8, shift: i32) -> u8 {
     let capital_a = 'A' as u8;
     let capital_z = 'Z' as u8;
 
+    let shift = shift.rem_euclid(26) as u8; 
+
     // Only apply shift if c is within a-z or A-Z, otherwise don't change it
-    todo!()
+    if c >= a && c <= z {
+        (c - a + shift) % 26 + a
+    } else if c >= capital_a && c <= capital_z {
+        (c - capital_a + shift) % 26 + capital_a
+    } else {
+        c
+    }
 }
 
 /// The ceasar_cipher should work for both upper case and lower case letters
@@ -49,16 +58,20 @@ fn shift_alphabet(c: u8, shift: i32) -> u8 {
 fn caesar_cipher(message: &str, shift: i32) -> String {
     // In rust &str is a wrapper over &[u8] which is a slice of bytes
     
-    let bytes = todo!();// Convert the message to a slice of bytes
+    let bytes = message.as_bytes(); // Convert the message to a slice of bytes
 
-    // let mut encrypted_bytes = Vec::new(); // Create a new vector to store the encrypted bytes
+    let mut encrypted_bytes = Vec::new(); // Create a new vector to store the encrypted bytes
     // for each byte apply the shift_alphabet function and collect them in encrypted_bytes
     // hint: use a for loop
 
-    let encrypted_message = todo!();
+    for &byte in bytes {
+        encrypted_bytes.push(shift_alphabet(byte, shift));
+    }
+
+    let encrypted_message =String::from_utf8(encrypted_bytes);
     // hint: Read https://doc.rust-lang.org/std/string/struct.String.html
     
-    encrypted_message // Return the encrypted message as a String
+    encrypted_message.unwrap() // Return the encrypted message as a String
 }
 
 // Example tests are given below
@@ -118,7 +131,7 @@ mod tests {
 
     #[test]
     fn test_shift_alphabet_a_neg1() {
-        assert_eq!(shift_alphabet(97, -1), 123);
+        assert_eq!(shift_alphabet(97, -1), 122);
     }
 
     #[test]
