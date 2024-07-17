24eb2a08f9e85bfa6ea0125a87c4d808bc07feb3
Vihiga Tyonum <withtvpeter@gmail.com>
2024-07-17 20:47:57 +0100

    use provided error enum in the crate instead of string errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, time};
 
-use bitcoincore_rpc::{json, jsonrpc::{self}, Auth, Client, RpcApi};
+use bitcoincore_rpc::{Error, json, jsonrpc::{self}, Auth, Client, RpcApi};
 use chrono::Duration;
 #[macro_use]
 extern crate lazy_static;
@@ -19,46 +19,48 @@ lazy_static! {
 // static client: Client = Client::new("url", Auth::UserPass("user".to_owned(), "password".to_owned())).unwrap();
 
 // TODO: Task 1
-fn time_to_mine(block_height: u64) -> Duration {
+fn time_to_mine(block_height: u64) -> Result<Duration, Error> {
     // * is a deref operator which invokes the Deref trait of the type RPC_CLIENT which was created
     // when the lazy macro is expanded
     // if a value has a static lifetime then it means that value lives as long as the program lives
     let rpc_client: &Client = &*RPC_CLIENT;
 
-    let block_count = rpc_client.get_block_count().expect("error getting block count");
+    let block_count = rpc_client.get_block_count()?;
 
     if block_height > block_count {
         panic!("Provided block height has not been mined");
     }
 
-    let given_block_hash = rpc_client.get_block_hash(block_height).expect("Error obtaining blockhash for given block");
-    let given_block_header = rpc_client.get_block_header(&given_block_hash).expect("error getting block header for given block");
+    let given_block_hash = rpc_client.get_block_hash(block_height)?;
+    let given_block_header = rpc_client.get_block_header(&given_block_hash)?;
 
     let prev_block_hash = given_block_header.prev_blockhash;
-    let prev_block_header = rpc_client.get_block_header(&prev_block_hash).expect("error getting previous block header");
+    let prev_block_header = rpc_client.get_block_header(&prev_block_hash)?;
 
     let time_diff = given_block_header.time - prev_block_header.time;
 
-    Duration::seconds(time_diff as i64)
+    Ok(Duration::seconds(time_diff as i64))
 }
 
 // TODO: Task 2
-fn number_of_transactions(block_height: u64) -> u16 {
+fn number_of_transactions(block_height: u64) -> Result<u16, Error> {
     let rpc_client = &*RPC_CLIENT;
 
-    let block_count = rpc_client.get_block_count().expect("error getting block count");
+    let block_count = rpc_client.get_block_count()?;
 
     if block_height > block_count {
         panic!("Provided block height has not been mined");
     }
     // let some_value = Box::new(4 as u32);
-    let block_hash = rpc_client.get_block_hash(block_height).expect("error getting given block height");
-    let block = rpc_client.get_block(&block_hash).expect("error getting block data");
+    let block_hash = rpc_client.get_block_hash(block_height)?;
+    let block = rpc_client.get_block(&block_hash)?;
 
     let transactions_num = block.txdata.len();
 
-    transactions_num as u16
+    Ok(transactions_num as u16)
 }
+
+
 
 fn main() {
     // you can use rpc_client here as if it was a global variable
@@ -104,7 +106,7 @@ mod tests {
 
     #[test]
     fn test_time_to_mine(){
-        let time_to_mine = time_to_mine(900);
+        let time_to_mine = time_to_mine(900).unwrap();
         assert!(time_to_mine.num_seconds()>= 0);
     }
 
@@ -112,14 +114,14 @@ mod tests {
     #[should_panic]
     fn test_time_to_mine_failure(){
         //set this to be above the block count
-        let time_to_mine = time_to_mine(1000);
+        let time_to_mine = time_to_mine(1000).unwrap();
         time_to_mine.num_seconds();
     }
 
     #[test]
     fn test_number_of_transactions(){
 
-        let num_of_txns = number_of_transactions(800);
+        let num_of_txns = number_of_transactions(800).unwrap();
 
         assert!(num_of_txns >= 1);
     }
